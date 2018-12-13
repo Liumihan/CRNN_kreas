698fe2b1de0e1f37c5ae3aea682f62ecccf27076
Liumihan <xiaofeiniuaichirou@foxmail.com>
2018-12-13 22:30:31 +0800

    在网络的最前端和两个gru之间加上了BN层，以及使用了he_normal初始化参数，最后得到了0.95的acc，并且过拟合现象明显减少了

--- src/vgg_bgru_ctc.py
+++ src/vgg_bgru_ctc.py
@@ -11,47 +11,56 @@ def model(is_training):
 
     initializer = keras.initializers.he_normal()
     regularizer = keras.regularizers.l2(0)
+    
     max_label_length = 12
-
-
     picture_width = 128
     picture_height = 32
+    num_classes = 11
+    
     #CNN part
     inputs = Input(shape=(picture_height, picture_width, 1), name='pic_inputs') # H×W×1 32*128*1
-    x = Conv2D(64, (3,3), strides=(1,1), padding="same", activation='relu', kernel_regularizer=regularizer, kernel_initializer=initializer, use_bias=True, name='conv2d_1')(inputs) # 32*128*64 
+    x = Conv2D(64, (3,3), strides=(1,1), padding="same", kernel_regularizer=regularizer, kernel_initializer=initializer, use_bias=True, name='conv2d_1')(inputs) # 32*128*64 
+    x = BatchNormalization(name="BN_1")(x)
+    x = Activation("relu", name="relu_1")(x)
     x = MaxPooling2D(pool_size=(2,2), strides=2, padding='valid', name='maxpl_1')(x) # 16*64*64
 
-    x = Conv2D(128, (3,3), strides=(1,1), padding="same", activation='relu', kernel_regularizer=regularizer, kernel_initializer=initializer, use_bias=True, name='conv2d_2')(x) # 16*64*128
+    x = Conv2D(128, (3,3), strides=(1,1), padding="same", kernel_regularizer=regularizer, kernel_initializer=initializer, use_bias=True, name='conv2d_2')(x) # 16*64*128
+    x = BatchNormalization(name="BN_2")(x)
+    x = Activation("relu", name="relu_2")(x)
     x = MaxPooling2D(pool_size=(2,2), strides=2, padding='valid', name='maxpl_2')(x) # 8*32*128
     
-    x = Conv2D(256, (3,3), strides=(1,1), padding="same", activation='relu', kernel_regularizer=regularizer, kernel_initializer=initializer, use_bias=True, name='conv2d_3')(x) # 8*32*256 
+    x = Conv2D(256, (3,3), strides=(1,1), padding="same", kernel_regularizer=regularizer, kernel_initializer=initializer, use_bias=True, name='conv2d_3')(x) # 8*32*256 
+    x = BatchNormalization(name="BN_3")(x)
+    x = Activation("relu", name="relu_3")(x)
     x = Conv2D(256, (3,3), strides=(1,1), padding="same", activation='relu', kernel_regularizer=regularizer, kernel_initializer=initializer, use_bias=True, name='conv2d_4')(x) # 8*32*256
+    x = BatchNormalization(name="BN_4")(x)
+    x = Activation("relu", name="relu_4")(x)    
     x = MaxPooling2D(pool_size=(2,1), strides=(2,1), name='maxpl_3')(x) # 4*32*256
 
     x = Conv2D(512, (3,3), strides=(1,1), padding="same", kernel_regularizer=regularizer, kernel_initializer=initializer, use_bias=True, name='conv2d_5')(x) # 4*32*512
-    x = BatchNormalization(axis=-1, name='BN_1')(x)
-    x = Activation("relu", name='relu_1')(x)
+    x = BatchNormalization(axis=-1, name='BN_5')(x)
+    x = Activation("relu", name='relu_5')(x)
     x = Conv2D(512, (3,3), strides=(1,1), padding="same", kernel_regularizer=regularizer, kernel_initializer=initializer, use_bias=True, name='conv2d_6')(x) # 4*32*512
-    x = BatchNormalization(axis=-1, name='BN_2')(x)
-    x = Activation("relu", name='relu_2')(x)
+    x = BatchNormalization(axis=-1, name='BN_6')(x)
+    x = Activation("relu", name='relu_6')(x)
     x = MaxPooling2D(pool_size=(2,1), strides=(2,1), name='maxpl_4')(x) # 2*32*512
     
-    x = Conv2D(512, (2,2), strides=(1,1), padding='same', kernel_regularizer=regularizer, activation='relu', kernel_initializer=initializer, use_bias=True, name='conv2d_7')(x) # 2*32*512
+    x = Conv2D(512, (2,2), strides=(1,1), padding='same', kernel_regularizer=regularizer, kernel_initializer=initializer, use_bias=True, name='conv2d_7')(x) # 2*32*512
+    x = BatchNormalization(name="BN_7")(x)
+    x = Activation("relu", name="relu_7")(x)    
     conv_otput = MaxPooling2D(pool_size=(2, 1), name="conv_output")(x) # 1*32*512
     
     # Map2Sequence part
-    rnn_input = Permute((2, 3, 1), name='permute')(conv_otput) # 32*512*1
-    rnn_output = TimeDistributed(Flatten(), name='for_flatten_by_time')(rnn_input) # 32*512
+    x = Permute((2, 3, 1), name='permute')(conv_otput) # 32*512*1
+    rnn_input = TimeDistributed(Flatten(), name='for_flatten_by_time')(x) # 32*512
 
     # RNN part
-    y = Bidirectional(GRU(256, return_sequences=True, kernel_regularizer=regularizer), name='GRU_1')(rnn_output) # 32*512
-    # y = BatchNormalization(name='BN_3')(y)
-    y = Bidirectional(GRU(256, return_sequences=True, kernel_regularizer=regularizer), name='GRU_2')(y) # 32*512
-
-                                                                        # 尝试跳过rnn层
-    y_pred = Dense(11, activation='softmax', name='y_pred', kernel_regularizer=regularizer)(y) # 32*11 这用来做evaluation 和 之后的test检测
+    y = Bidirectional(GRU(256, return_sequences=True, kernel_initializer=initializer, kernel_regularizer=regularizer), name='GRU_1')(rnn_input) # 32*512
+    y = BatchNormalization(name="BN_8")(y)
+    y = Bidirectional(GRU(256, return_sequences=True, kernel_initializer=initializer, kernel_regularizer=regularizer), name='GRU_2')(y) # 32*512
+                                                                       # 尝试跳过rnn层
+    y_pred = Dense(num_classes, activation='softmax', name='y_pred', kernel_initializer=initializer, kernel_regularizer=regularizer)(y) # 32*11 这用来做evaluation 和 之后的test检测
                                             # 在backend的实现ctc_loss的时候没有执行softmax操作所以这里必须要在使用softmax!!!!
-
 
     # Transcription part (CTC_loss part)
     y_true = Input(shape=[max_label_length], name='y_true')
